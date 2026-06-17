ce1c95e9528ceb0a04181d74f8869c6ccb323bfe
Adi Shankar <adishankar219@gmail.com>
2026-06-17 15:10:37 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,12 +1,15 @@
 const readline = require("readline");
 
+const shellOnline = true
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: "$ ",
 });
 
+
 rl.prompt();
 rl.on('line', (command)=>{
   console.log(`${command}: command not found`)
+  rl.prompt()
 })
