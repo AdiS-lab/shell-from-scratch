62fb15838616ae0bad9ac84dab731e08fd84f6aa
Adi Shankar <adishankar219@gmail.com>
2026-06-18 12:41:19 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -37,14 +37,13 @@ const rl = readline.createInterface({
         } //  end of for loop
         // console.log(hits)
         hits = [... new Set(hits)].sort() // handle duplicates create new set with hits, and then arr it
-        console.log(hits)
 
         if(!hits.length) process.stdout.write('\x07')
         else if(hits.length===1){
           return [hits, line]
         } 
         else if(hits === hits.filter(hit=>hit.includes(hits[0].trim()))){ // check if filtering by the first(root) gives you hits
-          return [hits[0].trim(), line]
+          return [[hits[0].trim()], line]
         } 
         else{
           tabCount += 1
