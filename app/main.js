dc6a94205b9b7fe4934dea59ba4329fab14ceb24
Adi Shankar <adishankar219@gmail.com>
2026-06-17 23:08:13 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -78,6 +78,7 @@ rl.on('line', (command)=>{
 
 
   if(normCom.includes('>') || normCom.includes('1>')){
+    if(normCom.includes('1>')) console.log('making it to normcom when 1>')
     const index = normCom.indexOf('>') || normCom.indexOf('1>')
     const targetFile = path.resolve(normCom[index+1])
     const output = execFileSync(normCom[0], normCom.slice(1,index),{encoding: 'utf8'})
