b9f7a8833378287e2541407bb49ef31e856abcd5
Adi Shankar <adishankar219@gmail.com>
2026-06-17 17:31:06 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -64,7 +64,7 @@ rl.on('line', (command)=>{
 
   else if(checkPath(directories, commandDivision[0])){
     const newPath = checkPath(directories, commandDivision[0])
-    const message = execFileSync(newPath, commandDivision.slice(1), {encoding: 'utf8'})
+    const message = execFileSync(commandDivision[0], commandDivision.slice(1), {encoding: 'utf8'})
     console.log(message)
   }
 
