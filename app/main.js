285b6535eb1494ffaca150307e86b58e982ab6d1
Adi Shankar <adishankar219@gmail.com>
2026-06-19 12:25:19 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -401,8 +401,14 @@ rl.on('line', (command)=>{
     const child1 = spawn(firstCommand[0], firstCommand.slice(1),{stdio:['inherit', 'pipe', 'inherit']})
     const child2 = spawn(secondCommand[0], secondCommand.slice(1),{stdio:['pipe', 'pipe', 'inherit']})
     child1.stdout.pipe(child2.stdin)
-    process.stdout.write('\n')
-    child2.stdout.pipe(process.stdout)
+
+    let output = ''
+    child2.on('data', (data) => output += data)
+
+    child2.on('close' , ()=>{
+      process.stdout.write('\n')
+      process.stdout.write(output)
+    })
   }
 
   else if(normCom.includes('complete') && normCom.includes('-p')){
