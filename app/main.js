799954c6757dd6dbd2e5ddd449cf019dec305416
Adi Shankar <adishankar219@gmail.com>
2026-06-19 12:36:08 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -399,9 +399,17 @@ rl.on('line', (command)=>{
     const firstCommand = normCom.slice(0, index)
     const secondCommand = normCom.slice(index+1)
     const child1 = spawn(firstCommand[0], firstCommand.slice(1),{stdio:['inherit', 'pipe', 'inherit']})
-    const child2 = spawn(secondCommand[0], secondCommand.slice(1),{stdio:['pipe', 'pipe', 'inherit']}) // inherit prints to the terminal, and 
+    const child2 = spawn(secondCommand[0], secondCommand.slice(1),{stdio:['pipe', 'pipe', 'inherit']}) // inherit prints to the terminal, since async process.stdout does the same
+    //handled last time with process.stdout.write('\n') 
     child1.stdout.pipe(child2.stdin)
-    child2.stdout.pipe(process.stdout)
+
+    let output = ''
+    child2.on('data', (data)=>output+=data)
+
+    child2.on('close', ()=>{
+      console.log(data)
+    })
+    // child2.stdout.pipe(process.stdout)
   }
 
   else if(normCom.includes('complete') && normCom.includes('-p')){
