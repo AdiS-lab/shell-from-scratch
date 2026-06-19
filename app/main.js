dd60191d1383c72101747fe7982e035997df8709
Adi Shankar <adishankar219@gmail.com>
2026-06-19 02:02:35 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -30,9 +30,8 @@ const rl = readline.createInterface({
           let args = [customCmd]
           args.push(normLine.at(-1))
           normLine.length>1 ? args.push(normLine.at(1)) : args.push('')
-          console.log(args)
           try{
-            const message = execFileSync(newCommands[customCmd],normLine, {encoding:"utf8", stdio: ['pipe', 'pipe', 'pipe']} )
+            const message = execFileSync(newCommands[customCmd],args, {encoding:"utf8", stdio: ['pipe', 'pipe', 'pipe']} )
             if(!message){
               process.stdout.write('\x07')
               return [[], line]
