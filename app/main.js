eb3b8f5e7929e2a76caee634d210b8dc58fc043d
Adi Shankar <adishankar219@gmail.com>
2026-06-19 21:27:54 -0700

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -583,7 +583,6 @@ rl.on('line', (command)=>{
       prevAppend.forEach((file)=>{
         if(filePath in file){
           const number = file[filePath].length
-          console.log(file[filePath])
           data = `${pastCommands.slice(number).join('\n')}\n`
 
 
